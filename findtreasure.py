d8ab4cb85e24d62ddd19dddd2adbc3d7744f9b66
Yeoul <121348869+yeoul2023@users.noreply.github.com>
2024-07-11 05:50:13 +0000

    Add new code

---
+++ findtreasure.py
@@ -0,0 +1,9 @@
+def treasure_game():
+    abc = ["a", "b", "c"]
+    line1 = ["0", "0", "0"]
+    line2 = ["0", "0", "0"]
+    line3 = ["0", "0", "0"]
+    
+    map = [line1, line2, line3]
+    print("Hining your treasure! X marks tje spot")
+    postion = input("").strip().lower()
