2864b9bcd0d3d205bb3aa7404bcc26ed9d9a8fda
Yeoul <121348869+yeoul2023@users.noreply.github.com>
2024-07-31 06:31:23 +0000

    all fixed

--- game/__init__.py
+++
@@ -1,4 +0,0 @@
-from .game_logic import ts_game
-from .player import Player
-from .file_operations import save_player_info, load_player_info
-from .timer import Timer
--- game/player.py
+++
@@ -1,7 +0,0 @@
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.play_time = None
-
-    def set_play_time(self, play_time):
-        self.play_time = play_time
